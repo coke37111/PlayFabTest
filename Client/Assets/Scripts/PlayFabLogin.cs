71d31563dc5053b21350b0483cb917f3f383decc
navigator <navigator@supervlabs.io>
2025-09-30 20:16:32 +0900

    PlayFab 초기 설정

---
+++ Client/Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,44 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    public void Start()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            throw new Exception("TitleId Is Null");
+        }
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+
+#if UNITY_EDITOR
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+#else
+        PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest
+        {
+            AndroidDeviceId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        }, result =>
+        {
+            Debug.Log("Logged in with Android device ID");
+        }, error =>
+        {
+            Debug.LogError("Error logging in with Android device ID: " + error.GenerateErrorReport());
+        });
+#endif
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
